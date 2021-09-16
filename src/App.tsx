e352bbf26800491f043126f853b355a506c87435
EvenW <yuhang0229@gmail.com>
2021-09-16 20:10:21 +1000

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import style from './App.module.css';
 
 import Robot from './components/Robot';
@@ -8,6 +8,10 @@ import robots from './mockdata/robots.json';
 function App() {
   return (
     <div className={style.app}>
+      <div className={style.appHeader}>
+        <img src={logo} alt="logo" className={style.appLogo} />
+        <h1>罗伯特机器人狂拽酷炫吊炸天</h1>
+      </div>
       <div className={style.robotList}>
         {robots.map((r) => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>

8cc81b85c0cbe9a5b18ad8c4ab332b01562d1db9
EvenW <yuhang0229@gmail.com>
2021-09-16 23:00:37 +1000

    add shoppingcart button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './assets/images/logo.svg';
 import style from './App.module.css';
 
+import ShoppingCart from './components/ShoppingCart';
 import Robot from './components/Robot';
 import robots from './mockdata/robots.json';
 
@@ -12,6 +13,7 @@ function App() {
         <img src={logo} alt="logo" className={style.appLogo} />
         <h1>罗伯特机器人狂拽酷炫吊炸天</h1>
       </div>
+      <ShoppingCart />
       <div className={style.robotList}>
         {robots.map((r) => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>
